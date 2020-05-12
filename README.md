30980e2cf0e897a9d46fb4566c4adaac0783d203
unknown <alsgks1218@naver.com>
2020-05-12 13:55:56 +0900

    completed nav tab and add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# vanilla javascript component exercise
+
+바닐라 자바스립트로 간단하게 만들어보는 컴포넌트들 입니다.
+
+## 현재 만들어 본 컴포넌트 입니다.
+
+[x] 탭 네비게이션
+
+---
--- nav-tab/index.js
+++ nav-tab/index.js
@@ -1,10 +1,23 @@
 const tabItems = document.getElementsByClassName("tab-items");
+const tabContents = document.getElementsByClassName("tab-contents");
 
 for (let i = 0; i < tabItems.length; i++) {
   tabItems[i].addEventListener("click", function () {
-    // 항목중에서 loop를 돌아서 current를 없앤다.
     const siblings = this.parentElement.children;
-    for (let j = 0; j < siblings.length; j++) {}
-    // 그리고 이놈한테 current를 달아준다.
+    for (let j = 0; j < siblings.length; j++) {
+      if (siblings[j].classList.contains("current")) {
+        siblings[j].classList.remove("current");
+        continue;
+      }
+    }
+    for (let k = 0; k < tabContents.length; k++) {
+      if (tabContents[k].classList.contains("current")) {
+        tabContents[k].classList.remove("current");
+      }
+    }
+
+    const tabNum = this.dataset.tab;
+    this.classList.add("current");
+    document.getElementById(tabNum).classList.add("current");
   });
 }
